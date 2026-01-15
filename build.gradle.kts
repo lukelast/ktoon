711910cbf2bc1f65c81ea15017b79b7683cef2ac
Luke <luke@lukelast.com>
2026-01-14 22:50:07 -0500

    Migrates to Android KMP plugin
    Migrates from the deprecated `android-library` plugin to the recommended `android-kmp` plugin for Kotlin Multiplatform projects.
    
    This ensures compatibility with the latest Android tooling and enables future KMP features.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlinx.benchmark) apply false
-    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.kmp) apply false
 }
 
 subprojects {
