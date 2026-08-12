7dee94817b9554185bd874f7b66739322002e72c
Luke <luke@lukelast.com>
2026-08-12 19:33:56 -0400

    remove deprecated kmp targets

--- ktoon/build.gradle.kts
+++ ktoon/build.gradle.kts
@@ -60,7 +60,6 @@ kotlin {
     // native target "tiers" here are taken from
     // https://kotlinlang.org/docs/native-target-support.html
     // tier 1
-    macosX64()
     macosArm64()
     iosSimulatorArm64()
     iosX64()
@@ -70,11 +69,9 @@ kotlin {
     linuxX64()
     linuxArm64()
     watchosSimulatorArm64()
-    watchosX64()
     watchosArm32()
     watchosArm64()
     tvosSimulatorArm64()
-    tvosX64()
     tvosArm64()
 
     // tier 3
